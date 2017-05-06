f14d8b72bd5fae7d63ef03664279108c8685a4b1
Jonathan-VB <jonathan@viralbamboo.com>
2017-05-06 16:23:38 +0100

    Setting Up Our Todo App
    - Set up our app with some HTML so we can see the transition from
    regular HTML to Angular enhanced HTML.

--- scripts/hello-world.js
+++
@@ -1,9 +0,0 @@
-// Leaving out dependencies array means Angular will
-// look for an existing app named 'todoListApp'.
-angular.module('todoListApp')
-.directive('helloWorld', function() {
-  return {
-    template: "This is the hello world directive!",
-    restrict: "E" // Only use as an element
-  }; // end return
-}); // end directive
