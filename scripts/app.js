dd51d1b046ef7aa9df7da0d0f83b27bca886bbf1
Jonathan-VB <jonathan@viralbamboo.com>
2017-05-06 16:36:51 +0100

    Creating a Controller
    - Learning how to create and use a controller.

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,7 @@
 // module(Name of Application, Empty Array(Defines dependencies)).
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log("Hello there! Thisis the helloWorld controller function in the main controller");
+  }
+}); // end controller
