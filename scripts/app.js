2d04ce59fc138db6a4b8719c2b3ef084b2d21915
Jonathan-VB <jonathan@viralbamboo.com>
2017-05-07 19:34:33 +0100

    ng-repeat to inject HTML for all elements
    - Using ng-repeat to create our todo list.

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,15 @@ angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope) {
   $scope.helloWorld = function() {
     console.log("Hello there! Thisis the helloWorld controller function in the main controller");
-  }
+  };
+
+  $scope.todos = [
+    {"name": "Clean the house"},
+    {"name": "Water the dog"},
+    {"name": "Feed the lawn"},
+    {"name": "Pay dem bills"},
+    {"name": "Run"},
+    {"name": "Swim"}
+  ];
+
 }); // end controller
